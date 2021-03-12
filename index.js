890efc367373707c8c4cc9574ed1823963a6e776
Harshit <geraharshitkumar@gmail.com>
2021-03-12 19:44:38 +0530

    view engine setup

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 8000;
 app.use('/', require('./routes/index'));
 
 
+// setting up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 
 
 app.listen(port, function(err){
