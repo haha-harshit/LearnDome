f33dd7ff6418e9bc9efeb4ded3514998d66c25ec
Harshit <geraharshitkumar@gmail.com>
2021-03-18 14:23:04 +0530

    added access to static files(css/js/images/sass)

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const port = 8000;
 // import the layout-lib
 const expressLayouts = require('express-ejs-layouts');
 
+//use static files ---> css/js files 
+app.use(express.static('./assets'));
+
 // use a particular layout ---> use it before routes to tell that these routes belong to a particular layout
 app.use(expressLayouts);
 
-
 // use express router
 app.use('/', require('./routes/index'));
-
 
 // setting up view engine
 app.set('view engine', 'ejs');
